e335cca6625469e643e636fca58ff2580e80ef3a
katinan20 <katine.toure95@gmail.com>
2023-08-01 11:15:35 +0000

    first commit

---
+++ src/test/java/com/katinan/katinan/KatinanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katinan.katinan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatinanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
